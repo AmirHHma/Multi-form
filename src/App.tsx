2f97b55813ca8177a471215c11b182fc7bbf17e5
Amir <mramirrhm@gmail.com>
2025-05-30 11:32:09 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Body from './body/Login'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+     <Body></Body>
+    </>
+  )
+}
+
+export default App
---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit"
+import payingMethod from './slices/payingMethod'
+import overallSummer from './slices/overallSummer'
+import completetion from './slices/completed'
+let store=configureStore({
+    reducer:{
+        setMethod:payingMethod,
+        overallSummer:overallSummer,
+        checkCompletion:completetion,
+    }
+})
+
+export default store;
+export type RootState=ReturnType<typeof store.getState>
